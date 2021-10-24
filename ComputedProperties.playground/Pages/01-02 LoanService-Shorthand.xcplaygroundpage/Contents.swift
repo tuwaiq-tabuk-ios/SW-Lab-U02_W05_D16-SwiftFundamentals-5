854f0670d28bf9367beae848e7976be2d3498fb0
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-10-24 15:44:29 +0300

    Finish my playground

--- ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
@@ -1,12 +1,31 @@
 //: [Previous](@previous)
-
-
 import Foundation
 
 struct LoanService {
-
+  
+  let currentMarketRate:Double
+  let loanBanckComission:Double = 0.01
+  
+//  var bankInteresRate:Double {
+//    return currentMarketRate + loanBanckComission
+//  }
+  
+  var bankInteresRate:Double {
+     currentMarketRate + loanBanckComission
+  }
+  
+  
+  func loan(quantity:Double) -> Double {
+    let interestToPay = quantity * bankInteresRate
+    print("Bank interest rate : \(bankInteresRate)")
+    print("The interest to pay for \(quantity) will be \(interestToPay)")
+    return interestToPay
+  }
+  
   
 }
+let loanService = LoanService(currentMarketRate: 0.02)
+loanService.loan(quantity: 6_000)
 
 
 
