6a8aa28d2fd52859b90fa57fdac827727abde5b8
apple <apple@MacBook-Pro-macbook.local>
2021-10-24 16:54:03 +0300

    Completed the Computed Properties

--- ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
@@ -1,9 +1,36 @@
+
+
 import Foundation
 
 struct LoanService {
-
   
+    let currentMarketRate: Double
+   let loanBankComission: Double = 0.01
+    
+  var bankInterestRate: Double {
+    
+get {
+      return currentMarketRate + loanBankComission
+    }
+    
 }
+
+func loan (quantity: Double) -> Double {
+  
+    let interestToPay = quantity * bankInterestRate
+    print("bank interest rate : \(bankInterestRate)")
+    print ("The interest to pay for \(quantity) will be \(interestToPay)")
+    
+    return interestToPay
+  }
+}
+
+let loanService = loanService (currentMarketRate: 0.02)
+loanService.loan(quantity: 6_000)
+
+
+
+
 
 
 
--- ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
@@ -1,13 +1,50 @@
-//: [Previous](@previous)
-
 import Foundation
 
 struct Employee {
-
+  let name : String
+  var annualSalary: Double
+  
+  
+  // في الذاكرة
+  var monthlySalary: Double {
+    get {
+      return annualSalary / Double(12)
+    }
     
+    set(newSalary) {
+      self.annualSalary = newSalary * Double(12)
+    }
+  }
+  
+  
+  var weeklySalary : Double {
+    
+    get {
+      return annualSalary / Double(7)
+    }
+    
+    set(newSalary2) {
+      self.annualSalary = newSalary2 * Double(7)
+    }
+  }
+  
+  
+  
 }
 
+var bob = Employee (name: "Bob" ,            // "انا استخدم الان "
+                    annualSalary: 6_000)
+
+//"the annualSalary of Bob is ........
+print("the annualSalary of Bob is \(bob.annualSalary)")
+
+//"the monthlySalary of Bob
+print("the monthlySalary of Bob is \(bob.monthlySalary) ")
 
 
+// "Bob has a monthly Salary rise of 100 per month
+bob.monthlySalary += 100
 
+// "The annual salary of Bob after the rise is
+print("The annual salary of Bob after the rise is \(bob.annualSalary) ")
 //: [Next](@next)
