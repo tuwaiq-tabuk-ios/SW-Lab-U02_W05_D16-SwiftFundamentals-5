65b66ba00093ef64f0f6df551db3fbff5c514010
macbook air <macbookair@Danah.local>
2021-10-24 16:03:45 +0300

    complet the homework

--- ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
@@ -1,9 +1,29 @@
 import Foundation
 
 struct LoanService {
-
   
+  let currentMarketRate: Double
+  let loanBankComission: Double = 0.01
+  
+  var bankInteresRate: Double {
+    get {
+      return currentMarketRate + loanBankComission
+    }
+  }
+  
+  
+  func loan(quantity: Double) -> Double {
+    let interestToPay = quantity * bankInteresRate
+    print("Bank interest rate: \(bankInteresRate)")
+    print("The interest to pay for \(quantity) will be \(interestToPay)")
+    return interestToPay
+  }
 }
+let loanService = LoanService(currentMarketRate: 0.02)
+loanService.loan(quantity: 6_000)
+
+
+
 
 
 
--- ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
@@ -3,10 +3,34 @@
 import Foundation
 
 struct Employee {
-
-    
+  
+  let name: String
+  var annualSalary: Double
+  
+  var monthlySalary: Double {
+    get {
+      return annualSalary / Double(12)
+    }
+    set(newSalary) {
+      self.annualSalary = newSalary * Double(12)
+    }
+  }
+  
 }
 
+var bob = Employee(name: "bob", annualSalary: 30_000 )
+//"The annual Salary of bob is .....
+print("The annual Salary of bob is \(bob.annualSalary)")
+
+
+//"The monthly Salary of bob is .....
+print("The monthly Salary of bob is\(bob.monthlySalary)")
+
+// Bob has a monthly salary rise of 100 per month
+
+bob.monthlySalary += 100
+//"The annual salary of Bob after the rise is ...
+print("The annual salary of Bob after the rise is \(bob.annualSalary)")
 
 
 
