a71fedba8e213ac70aeec5c620dd5a0b47573625
Mohammed <mohammed@Mohammeds-MacBook-Air.local>
2021-10-24 16:10:28 +0300

    I Finished the task

--- ComputedProperties.playground/Pages/02-01 Employee.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-01 Employee.xcplaygroundpage/Contents.swift
@@ -3,9 +3,35 @@
 import Foundation
 
 struct Employee {
-
+    let name : String
+    var annualSalary: Double
     
+    var monthlySalary: Double{
+    get {
+        return annualSalary / Double(12)
+    }
+    
+
+    set(newSalary) {
+        self.annualSalary = newSalary * Double(12)
 }
+        
+}
+}
+var Mohammed = Employee(name: "Mohammed", annualSalary: 60)
+print("The anuual salary of mohammed\(Mohammed.annualSalary)")
+print("The anuual monthly of mohammed\(Mohammed.monthlySalary)")
+Mohammed.monthlySalary += 100
+
+print("The annual salary of Mohammed after the rise is \(Mohammed.annualSalary)")
+//"The annual salary of Bob is ...
+//"The monthly salary of Bob is ...
+
+//"Bob has a monthly salary rise of 100 per month
+// "The annual salary of Bob after the rise is ...
+
+
+
 
 
 
--- ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
@@ -1,10 +1,46 @@
 //: [Previous](@previous)
-
 import Foundation
+
+struct Employee {
+    
+    
+    
+    let name : String
+    var annualSalary: Double
+    
+    var monthlySalary: Double{
+        get {
+            annualSalary / Double(12)
+        }
+        set(newSalary) {
+            annualSalary = newSalary * Double(12)
+        }
+    }
+    
+    var weeklySalary: Double{
+        get {
+            annualSalary / Double(12*4)
+        }
+        set {
+            annualSalary = newValue * Double(12*4)
+        }
+    }
+    
+}
+var Mohammed = Employee(name: "Mohammed", annualSalary: 60)
+print("The anuual salary of mohammed is \(Mohammed.annualSalary)")
+print("The monthly of mohammed is \(Mohammed.monthlySalary)")
+print("Mohammed has a monthly salary of \(Mohammed.monthlySalary += 100) per month")
+print("The annual salary of Mohammed after the rise is \(Mohammed.annualSalary)")
 
 /*:
   - callout(Exercise): Create a new **_Read & Write computed property_** that calculates the weekly salary. Set a new rise of salary, but in this case the amount is given per week. After thatr, print the Bob's weekly salary.
 */
+print("The weekly of mohammed is \(Mohammed.weeklySalary)")
+
+Mohammed.weeklySalary += 20
+print("The annual salary of Mohammed after the rise is \(Mohammed.annualSalary)")
+
 
 //: [Next](@next)
 
