0fc2fbeae81adc2786d498d341a6ab84ae84f6a8
reema <reemamousa@MacBook-Pro-Reema.local>
2021-10-24 16:22:53 +0300

    creat Tasks

--- ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-02 Employee-Shorhand_setter.xcplaygroundpage/Contents.swift
@@ -1,13 +1,35 @@
 //: [Previous](@previous)
+//: [Previous](@previous)
 
 import Foundation
 
 struct Employee {
-
+  let name :String
+  var annualSalary :Double
+  var monthlySalaray : Double{
+    get{
+      return annualSalary / Double(12)
+      
+    }
+    set(newSalaray) {
+      self.annualSalary = newSalaray * Double(12)
     
+    }
+  }
 }
+var bob = Employee(name: "Bob",annualSalary:6_000)
 
+// the annual Salary of Bob is  ...
 
+// the monthly Salary of Bob is  ...
+
+print ("the annual Salary of \(bob.name) is  , \(bob.annualSalary) and monthly Salary is \(bob.monthlySalaray)")
+
+// Bob has a monthly salaray rise of 100 per month
+bob.monthlySalaray += 100
+
+// "the annual salaray of Bob after the rise is ...
+print("The annual salary of Bob after the rise is \(bob.annualSalary)")
 
 
 //: [Next](@next)
