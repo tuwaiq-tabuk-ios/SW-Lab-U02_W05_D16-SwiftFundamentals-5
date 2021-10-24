4f7f46d95a10829ddb6ea6aaf0b6c9d6cd7060b7
Atheer abdullah <91871760+atheerab@users.noreply.github.com>
2021-10-24 19:22:22 +0300

    completed

--- ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
@@ -2,9 +2,24 @@ import Foundation
 
 struct LoanService {
 
+  let currentMarketRate: Double
+  let loanBankComission: Double = 0.01
   
+  var bankIntrerestRate: Double {
+    get {
+      return currentMarketRate + loanBankComission
+    }
+  }
+  func loan(quantity: Double) -> Double {
+    let interestTopay = quantity * bankIntrerestRate
+    print("bank interest rate: \(bankIntrerestRate)")
+    print("the interest to pay for \(quantity) will be \(interestTopay)")
+  return interestTopay
 }
-
+}
+  let loanService = LoanService(currentMarketRate: 0.02)
+loanService.loan(quantity: 6_000)
+  
 
 
 
--- ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
@@ -5,8 +5,28 @@ import Foundation
 
 struct LoanService {
 
+  let currentMarketRate: Double
+  let loanBankComission: Double = 0.01
   
+ // var bankIntrerestRate: Double {
+  //    return currentMarketRate + loanBankComission
+  //
+  var bankIntrerestRate: Double {
+   currentMarketRate + loanBankComission
+  }
+  func loan(quantity: Double) -> Double {
+    let interestTopay = quantity * bankIntrerestRate
+    print("bank interest rate: \(bankIntrerestRate)")
+    print("the interest to pay for \(quantity) will be \(interestTopay)")
+  return interestTopay
 }
+}
+  let loanService = LoanService(currentMarketRate: 0.02)
+loanService.loan(quantity: 6_000)
+  
+
+
+
 
 
 
