504183b3ba5c96d9257d1c3966977d2c4b29f782
Maha S <mahas@Mahas-MacBook-Air.local>
2021-10-24 19:35:42 +0300

    Computed Properties playground

--- ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
@@ -3,8 +3,29 @@
 import Foundation
 
 /*:
-  - callout(Exercise): Create a new **_Read-Only computed property_** that calculates the properties `street`, `streetNumber`, `city` and `postalCode`. The new **_Read-Only computed property_** is called `address` . This computed propeties contains the following string `"\(street) \(streedNumber) \(postalCode), \(city)"`. Create a new instance of Employee giving in its inicializer all the address information separatelly, and after that, print the employee's address.
-*/
+ - callout(Exercise): Create a new **_Read-Only computed property_** that calculates the properties `street`, `streetNumber`, `city` and `postalCode`. The new **_Read-Only computed property_** is called `address` . This computed propeties contains the following string `"\(street) \(streedNumber) \(postalCode), \(city)"`. Create a new instance of Employee giving in its inicializer all the address information separatelly, and after that, print the employee's address.
+ */
+struct Employee {
+  
+  var street: String
+  var streetNumber: Int
+  var city: String
+  var postalCode: Int
+  
+  var address: String {
+    get{
+      return("\(street) \(streetNumber) \(postalCode), \(city)")
+    }
+  }
+}
+
+var employeeAdress = Employee(street:"50 Street",
+                              streetNumber: 142,
+                              city: "Tabuk",
+                              postalCode: 1432)
+
+print("The emplyee's address is: \(employeeAdress.address)")
+
 
 
 
