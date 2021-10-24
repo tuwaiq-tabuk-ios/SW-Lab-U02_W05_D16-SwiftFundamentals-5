09890ed7a8a6b0ee1c920c99ac92a580aca143d7
ahlam <khald@MacBook-Air-khald.local>
2021-10-24 16:56:03 +0300

    SwiftFundamentals-5

--- ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-01 LoanService.xcplaygroundpage/Contents.swift
@@ -2,11 +2,24 @@ import Foundation
 
 struct LoanService {
 
-  
+    let currentMarkeRate: Double
+    let loanBankComission: Double = 0.01
+    var bankInterestRate: Double {
+    get {
+        return currentMarkeRate +
+        loanBankComission
+    }
 }
 
-
-
-
+    func loan(quantity: Double) -> Double {
+        let intetestToPay = quantity * bankInterestRate
+        print("Bank inerest rate:\(bankInterestRate)")
+        print("The inerest to pay for \(quantity) will be \(intetestToPay)")
+        return intetestToPay
+    }
+}
+    let loanService = LoanService(currentMarkeRate: 0.02)
+           loanService.loan(quantity: 6.000)
 //: [Next](@next)
+
 
