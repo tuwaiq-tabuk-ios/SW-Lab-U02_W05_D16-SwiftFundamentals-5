b4e44cf3a61c1971918ea6c6a5498121e4572644
hajer <h@MacBook-Pro-hajer.local>
2021-10-24 17:07:53 +0300

    compieat

--- ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
@@ -2,12 +2,67 @@
 
 import Foundation
 
-/*:
-  - callout(Exercise): Create a new **_Read & Write computed property_** that calculates the weekly salary. Set a new rise of salary, but in this case the amount is given per week. After thatr, print the Bob's weekly salary.
-*/
+
+
+struct Employee {
+    let name: String
+    var annualSalary: Double
+    
+    var monthlySalary: Double {
+        get {
+            return annualSalary / Double(12)
+        }
+        set(newSalary) {
+            self.annualSalary = newSalary * Double(12)
+        }
+    }
+    
+    var WeeklySalary: Double {
+        get {
+            return annualSalary / Double(52)
+        }
+        set(newSalary) {
+            self.annualSalary = newSalary * Double(52)
+        }
+    }
+    
+}
+
+var bob = Employee(name: "Bob", annualSalary: 6_000)
+
+// The annumul salary of bob is ...
+print("The annumul salary of bob is \(bob.annualSalary)")
+
+// The monthly salary of bob is ...
+print(" The monthly salary of bob is \(bob.monthlySalary)")
+
+// Bob has a monthly salary rise of 100 per month\
+bob.monthlySalary = bob.monthlySalary + 100
+
+//  The annual salary of bob after the rise
+print("The annual salary of bob after the rise \(bob.annualSalary)")
+
+//"The Weekly Salary of Bob is ...
+print("The Weekly Salary of Bob is \(bob.WeeklySalary)")
+
+
+bob.WeeklySalary += 10
+
+print(print("The annual salary of bob after the rise \(bob.annualSalary)")
+
+
+
 
 //: [Next](@next)
 
 
+
+/*:
+ - callout(Exercise): Create a new **_Read & Write computed property_** that calculates the weekly salary. Set a new rise of salary, but in this case the amount is given per week. After thatr, print the Bob's weekly salary.
+ */
+
+//: [Next](@next)
+
+)
 
 
--- ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
@@ -2,9 +2,60 @@
 
 import Foundation
 
+struct Employee {
+    let name: String
+    
+    var annualSalary: Double
+    
+    var monthlySalary: Double {
+        get {
+            return annualSalary / Double(12)
+        }
+        set(newSalary) {
+            self.annualSalary = newSalary * Double(12)
+        }
+    }
+    
+    var weeklySalary: Double {
+        get {
+            return annualSalary / Double(48)
+        }
+        set(newSalary) {
+            self.annualSalary = newSalary * Double(48)
+        }
+    }
+    
+    let street: String
+    let streetNumber: String
+    let postalCode: String
+    let city: String
+    let region: String
+    
+    var address : String {
+        get {
+            return "\(streetNumber) \(postalCode) \(city) (\(region)) \(street) "
+        }
+    }
+}
+
+
+var anna = Employee(name: "Anna",
+                    annualSalary: 6_000,
+                    street: "King khalid street",
+                    streetNumber: "30",
+                    postalCode: "71421",
+                    city: "Tabuk",
+                    region: "Tabuk")
+
+print(anna.city)
+print(anna.streetNumber)
+
+print("The employee address is \(anna.address)")
+
 /*:
-  - callout(Exercise): Create a new **_Read-Only computed property_** that calculates the properties `street`, `streetNumber`, `city` and `postalCode`. The new **_Read-Only computed property_** is called `address` . This computed propeties contains the following string `"\(street) \(streedNumber) \(postalCode), \(city)"`. Create a new instance of Employee giving in its inicializer all the address information separatelly, and after that, print the employee's address.
-*/
+ - callout(Exercise): Create a new **_Read-Only computed property_** that calculates the properties `street`, `streetNumber`, `city` and `postalCode`. The new **_Read-Only computed property_** is called `address` . This computed propeties contains the following string `"\(street) \(streedNumber) \(postalCode), \(city)"`. Create a new instance of Employee giving in its inicializer all the address information separatelly, and after that, print the employee's address.
+ */
+
 
 
 
