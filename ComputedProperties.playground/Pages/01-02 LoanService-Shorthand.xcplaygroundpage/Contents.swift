fab094e942ec38478438ad2ce1cc8540ea1a9383
mohammed <mhmdalsbwty@gmail.com>
2021-10-24 17:44:47 +0300

    SW-Lab-U02_W05_D16-SwiftFundamentals-5

--- ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
@@ -4,6 +4,28 @@
 import Foundation
 
 struct LoanService {
+    
+    let currentMarketRate: Double
+    let longBankComission: Double = 0.01
+    
+    var bankInterestRate: Double {
+        
+        
+        currentMarketRate + longBankComission
+        
+    }
+    
+    func long(quentity: Double) -> Double {
+        
+        let interestToPay = quentity * bankInterestRate
+        
+        print("Bank interest rate : \(bankInterestRate)")
+        print("The ihterest to pay for \(quentity) will be \(interestToPay)")
+        
+        return interestToPay
+        
+        
+    }
 
   
 }
--- ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-03 Employee-Weekly_salary.xcplaygroundpage/Contents.swift
@@ -6,8 +6,56 @@ import Foundation
   - callout(Exercise): Create a new **_Read & Write computed property_** that calculates the weekly salary. Set a new rise of salary, but in this case the amount is given per week. After thatr, print the Bob's weekly salary.
 */
 
+struct Employee {
+    let name : String
+    var annualSalary: Double
+   
+    var monthlySalary: Double {
+        get {
+            
+             annualSalary / Double(52)
+            
+        }
+        set {
+            
+            self.annualSalary = newValue * Double(12)
+            
+        }
+    }
+    var weeklySslary: Double{
+        get {
+            
+             annualSalary / Double(52)
+            
+        }
+        set {
+            
+            self.annualSalary = newValue * Double(52)
+            
+    }
+    
+}
+}
+var mohammed = Employee(name: "mohammed", annualSalary: 12000)
+
+// " The annoal salary of Bob is ...
+print("The annoal salary of mohammed is\(mohammed.annualSalary)")
+// " The monthly salary of Bob is ...
+print("The monthly salary of mohammed is\(mohammed.monthlySalary)")
+// " Bob has a monthly salary rise of 100 per month
+mohammed.monthlySalary += 100
+// " The annual salary of Bob after the rise is .....
+print("The annoal salary of mohammed is\(mohammed.annualSalary)")
+
+
+
+
 //: [Next](@next)
 
+print("The annoal weekle of mohammed is\(mohammed.weeklySslary)")
 
 
+mohammed.weeklySslary += 10
+
+print("The annoal weekle of mohammed is\(mohammed.weeklySslary)")
 
