8d0a9df368e29d18137d9fcf96c87173cc4a8ca0
Ameera BA <alqraniameera@outlook.com>
2021-10-24 15:46:23 +0300

    Finish

--- ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/01-02 LoanService-Shorthand.xcplaygroundpage/Contents.swift
@@ -4,9 +4,29 @@
 import Foundation
 
 struct LoanService {
-
   
+  let currentMarketRate: Double
+  let loanBankComission: Double = 0.01
+  
+  //  var bankInterestRate: Double {
+  //      return currentMarketRate + loanBankComission
+  //    }
+  
+  var bankInterestRate: Double {
+     currentMarketRate + loanBankComission
+  }
+  
+  func loen(quantity:Double) -> Double {
+    let interestToPay = quantity * bankInterestRate
+    print("Bank interest rate: \(bankInterestRate)")
+    print("The interest to pay for \(quantity) will be \(interestToPay)")
+    
+    return interestToPay
+  }
 }
+let loanService = LoanService(currentMarketRate: 0.02)
+loanService.loen(quantity: 6_000)
+
 
 
 
--- ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
+++ ComputedProperties.playground/Pages/02-04 Employee-Address.xcplaygroundpage/Contents.swift
@@ -5,9 +5,21 @@ import Foundation
 /*:
   - callout(Exercise): Create a new **_Read-Only computed property_** that calculates the properties `street`, `streetNumber`, `city` and `postalCode`. The new **_Read-Only computed property_** is called `address` . This computed propeties contains the following string `"\(street) \(streedNumber) \(postalCode), \(city)"`. Create a new instance of Employee giving in its inicializer all the address information separatelly, and after that, print the employee's address.
 */
+print(" - - - Exercise 1 - - - ")
 
+struct Employee {
+  let street: String
+  let streetNumber: Int
+  let city: String
+  let postalCode: Int
+  
+  var address: String {
+    get{
+      return ("\(street),\(streetNumber),\(postalCode),\(city)")
+    }
+  }
+}
 
+var Ameera = Employee(street: "Muadh bin Jabal", streetNumber: 33, city: "Tabuk", postalCode: 47484)
 
-
-
-
+print("The address of the employee Ameera is \(Ameera.address)")
